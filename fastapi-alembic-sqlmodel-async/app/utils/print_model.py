af424b8a6f35181c789344903be9fc4f59b3eeba
jonra1993 <jon_ra@hotmail.es>
2022-05-15 20:47:00 -0500

    Add print model function for complex models

---
+++ fastapi-alembic-sqlmodel-async/app/utils/print_model.py
@@ -0,0 +1,11 @@
+from fastapi.encoders import jsonable_encoder
+from typing import List, TypeVar
+from sqlmodel import SQLModel
+
+ModelType = TypeVar("ModelType", bound=SQLModel)
+
+def print_model(text: str = '', model: ModelType = []):
+    """
+        It prints sqlmodel responses for complex relationship models.
+    """
+    return print(text, jsonable_encoder(model))
