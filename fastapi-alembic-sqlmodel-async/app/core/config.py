2770322325dbbfc5aaaf04e79b57bb8a9877b4af
jonra1993 <jon_ra@hotmail.es>
2022-12-17 20:15:18 -0500

    Edit get_db

--- fastapi-alembic-sqlmodel-async/app/core/config.py
+++ fastapi-alembic-sqlmodel-async/app/core/config.py
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
     API_VERSION: str = "v1"
     API_V1_STR: str = f"/api/{API_VERSION}"
     PROJECT_NAME: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 180
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 1  #1 hour
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 100 #100 days
     DATABASE_USER: str
     DATABASE_PASSWORD: str
     DATABASE_HOST: str
