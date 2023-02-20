73f1da075ffb677d2b63f27b30cb0e4564845319
Jim Chng, Ph.D <jymchng@hotmail.com>
2023-02-20 15:59:37 +0800

    Update partial.py
    add the following 2 lines:
    
    ```
    if _cls.__fields__[field].default:
                _cls.__fields__[field].default = None
    ```
    
    the `optional` decorator is meant for `IUpdateSchemaType` and an 'UpdateSchema' should have all its fields' default set to None.

--- backend/app/app/utils/partial.py
+++ backend/app/app/utils/partial.py
@@ -9,6 +9,8 @@ def optional(*fields):
     def dec(_cls):
         for field in fields:
             _cls.__fields__[field].required = False
+            if _cls.__fields__[field].default:
+                _cls.__fields__[field].default = None
         return _cls
 
     if fields and inspect.isclass(fields[0]) and issubclass(fields[0], BaseModel):
