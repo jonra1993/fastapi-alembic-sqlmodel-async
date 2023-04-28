faae42442f1bca8bcd199d1e70402c5b2ae36e0c
jonra1993 <jon_ra@hotmail.es>
2023-04-27 20:32:16 -0500

    Add chatgpt websocket

--- backend/app/app/schemas/common_schema.py
+++ backend/app/app/schemas/common_schema.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from uuid import UUID
+from app.utils.uuid6 import uuid7
+from pydantic import BaseModel, validator
 from enum import Enum
 from app.schemas.role_schema import IRoleRead
 
@@ -21,3 +23,35 @@ class IOrderEnum(str, Enum):
 class TokenType(str, Enum):
     ACCESS = "access_token"
     REFRESH = "refresh_token"
+
+class IUserMessage(BaseModel):
+    """User message schema."""
+
+    user_id: UUID | None
+    message: str
+
+class IChatResponse(BaseModel):
+    """Chat response schema."""
+    id:  str
+    message_id: str
+    sender: str
+    message: str
+    type: str
+    
+    @validator("id", "message_id", pre=True, allow_reuse=True)
+    def check_ids(cls, v):        
+        if v == "" or v == None:            
+            return str(uuid7())
+        return v
+    
+    @validator("sender")
+    def sender_must_be_bot_or_you(cls, v):
+        if v not in ["bot", "you"]:
+            raise ValueError("sender must be bot or you")
+        return v
+
+    @validator("type")
+    def validate_message_type(cls, v):
+        if v not in ["start", "stream", "end", "error", "info"]:
+            raise ValueError("type must be start, stream or end")
+        return v
